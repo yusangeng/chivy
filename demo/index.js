73668b2c9c8824cf693edcbe5eb189aa5193ea91
yusangeng <yusangeng@outlook.com>
2018-01-05 00:56:19 +0800

    修改demo

--- demo/index.js
+++ demo/index.js
@@ -1,12 +1,21 @@
-var logger = new Chivy('fooModule/SomeClass')
-var logger2 = new Chivy('barModule/AnotherClass')
 
-logger.debug('0000.')
-logger.info('1111.')
-logger.warn('2222.')
-logger.error('3333.')
+var logger_App_Main = new Chivy('App/Main')
+var logger_App_Timeline = new Chivy('App/Timeline')
 
-logger2.debug('0000.')
-logger2.info('1111.')
-logger2.warn('2222.')
-logger2.error('3333.')
+var logger_UIComponent_Button = new Chivy('UIComponents/Button')
+var logger_UIComponent_Select = new Chivy('UIComponents/Select')
+var logger_UIComponent_GroupBox = new Chivy('UIComponents/GroupBox')
+
+function run () {
+  var args = Array.prototype.slice.call(arguments)
+
+  args.forEach(function (logger) {
+    logger.debug('This is a debug log.')
+    logger.info('This is a info log.')
+    logger.warn('This is a warning log.')
+    logger.error('This is a error log.')
+  })
+}
+
+run(logger_App_Main, logger_App_Timeline,
+  logger_UIComponent_Button, logger_UIComponent_Select, logger_UIComponent_GroupBox)
