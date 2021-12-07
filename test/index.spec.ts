474d74c9196dd6d180a363538b2d9dc92f042838
yusangeng <yusangeng@outlook.com>
2021-12-07 18:47:43 +0800

    test: 格式化测试代码

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,48 +1,48 @@
 /* global describe it */
-import chai from 'chai'
-import Logger from '../src/Logger'
+import chai from "chai";
+import Logger from "../src/Logger";
 
-chai.should()
+chai.should();
 
-describe('chivy', () => {
-  describe('default config', () => {
-    it('should be printed', async () => {
-      const log = new Logger('foo/bar')
+describe("chivy", () => {
+  describe("default config", () => {
+    it("should be printed", async () => {
+      const log = new Logger("foo/bar");
 
-      log.warn('xxx').should.be.eq(true)
-      log.error('xxx').should.be.eq(true)
-    })
+      log.warn("xxx").should.be.eq(true);
+      log.error("xxx").should.be.eq(true);
+    });
 
-    it('should NOT be printed', async () => {
-      const log = new Logger('foo/bar')
+    it("should NOT be printed", async () => {
+      const log = new Logger("foo/bar");
 
-      log.debug('xxx').should.be.eq(false)
-      log.info('xxx').should.be.eq(false)
-    })
-  })
+      log.debug("xxx").should.be.eq(false);
+      log.info("xxx").should.be.eq(false);
+    });
+  });
 
-  describe('custom config', () => {
+  describe("custom config", () => {
     const conf = {
-      'chivy-level': 3,
-      'chivy-modules': ['foo/bar']
-    }
+      "chivy-level": 3,
+      "chivy-modules": ["foo/bar"]
+    };
 
-    it('should be printed', async () => {
-      const log = new Logger('foo/bar', conf)
-      log.error('error').should.be.eq(true)
-    })
+    it("should be printed", async () => {
+      const log = new Logger("foo/bar", conf);
+      log.error("error").should.be.eq(true);
+    });
 
-    it('should NOT be printed because of level', async () => {
-      const log = new Logger('foo/bar', conf)
+    it("should NOT be printed because of level", async () => {
+      const log = new Logger("foo/bar", conf);
 
-      log.debug('debug').should.be.eq(false)
-      log.info('info').should.be.eq(false)
-      log.warn('warn').should.be.eq(false)
-    })
+      log.debug("debug").should.be.eq(false);
+      log.info("info").should.be.eq(false);
+      log.warn("warn").should.be.eq(false);
+    });
 
-    it('should NOT be printed because of module', async () => {
-      const log = new Logger('foo/baz', conf)
-      log.error('error').should.be.eq(false)
-    })
-  })
-})
+    it("should NOT be printed because of module", async () => {
+      const log = new Logger("foo/baz", conf);
+      log.error("error").should.be.eq(false);
+    });
+  });
+});
