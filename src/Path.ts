e46ae8f82011953079945e0b7810e67c962f94b3
yusangeng <yusangeng@outlook.com>
2025-01-15 08:10:54 +0800

    refactor: 优化代码

--- src/Path.ts
+++ src/Path.ts
@@ -23,8 +23,8 @@ const isString = (v: any) => typeof v === "string";
  * @class Path
  */
 export default class Path {
-  sections_: Array<string>;
-  str = "";
+  private segments_: Array<string>;
+  private str_ = "";
 
   /**
    * 路径分段列表
@@ -33,29 +33,30 @@ export default class Path {
    *
    * @memberof Path
    */
-  get sections() {
-    return this.sections_.slice();
+  get segments() {
+    return this.segments_.slice();
   }
 
   /**
    * Creates an instance of Path.
    *
-   * @param {string|Path} str 路径字符串或其他路径对象, 不可为空字符串
+   * @param {string|Path} source 路径字符串或其他路径对象, 不可为空字符串
    *
    * @memberof Path
    * @private
    */
-  constructor(str: string) {
-    const pathStr = (this.str = (str || "").toString());
+  constructor(source: string | Path) {
+    const pathStr = typeof source === "string" ? source : source.toString();
 
-    this.sections_ = pathStr
+    if (!pathStr || !pathStr.trim()) {
+      console.warn(`不建议使用空字符串("${pathStr}")构造Path实例.`);
+    }
+
+    this.str_ = pathStr;
+    this.segments_ = pathStr
       .split(/\/+/)
       .map(el => el.trim())
       .filter(el => el.length);
-
-    if (!this.sections_.length) {
-      console.warn(`不建议使用空字符串("${pathStr}")构造Path实例.`);
-    }
   }
 
   /**
@@ -67,7 +68,7 @@ export default class Path {
    * @instance
    */
   toString(): string {
-    return this.str;
+    return this.str_;
   }
 
   /**
@@ -101,11 +102,11 @@ export default class Path {
       throw new Error("被匹配的路径不可包含通配符(*或**).");
     }
 
-    return sectionListMatch(this.sections, (otherPath as Path).sections);
+    return segmentListMatch(this.segments, (otherPath as Path).segments);
   }
 }
 
-enum SectionMatchResult {
+enum SegmentMatchResult {
   NotMatch = -1,
   Match = 0,
   NextMatch = 1
@@ -118,22 +119,22 @@ enum SectionMatchResult {
  * @param {Array<string>} right
  * @returns {boolean} 匹配返回true, 否则返回false
  */
-function sectionListMatch(left: Array<string>, right: Array<string>): boolean {
+function segmentListMatch(left: Array<string>, right: Array<string>): boolean {
   while (left.length && right.length) {
     const currentLeft = left.shift() as string;
-    const nextLeft = left[0]; // 如果数组里没元素则为undefined, sectionMatch会认为没有nextLeft可用
+    const nextLeft = left[0]; // 如果数组里没元素则为undefined, segmentMatch会认为没有nextLeft可用
     const currentRight = right.shift() as string;
 
-    const sectionResult = sectionMatch(currentLeft, nextLeft, currentRight);
+    const segmentResult = segmentMatch(currentLeft, nextLeft, currentRight);
 
-    if (sectionResult === SectionMatchResult.NotMatch) {
+    if (segmentResult === SegmentMatchResult.NotMatch) {
       return false;
-    } else if (sectionResult === SectionMatchResult.Match) {
+    } else if (segmentResult === SegmentMatchResult.Match) {
       if (currentLeft === doubleWildcard) {
         // 把通配符放回去
         left.unshift(currentLeft);
       }
-    } else if (sectionResult === SectionMatchResult.NextMatch) {
+    } else if (segmentResult === SegmentMatchResult.NextMatch) {
       if (currentLeft === doubleWildcard) {
         // 此时需要要把下一个元素取出来, 下次匹配的时候直接从下下个开始
         left.shift();
@@ -159,31 +160,31 @@ function sectionListMatch(left: Array<string>, right: Array<string>): boolean {
  * @param {string} left
  * @param {string} nextLeft
  * @param {string} right
- * @returns {SectionMatchResult} left匹配right返回Match, nextLeft匹配right返回NextMatch, 不匹配返回NotMatch
+ * @returns {SegmentMatchResult} left匹配right返回Match, nextLeft匹配right返回NextMatch, 不匹配返回NotMatch
  */
-function sectionMatch(
+function segmentMatch(
   left: string,
   nextLeft: string,
   right: string
-): SectionMatchResult {
+): SegmentMatchResult {
   if (left === wildcard) {
     // 通配符
-    return SectionMatchResult.Match;
+    return SegmentMatchResult.Match;
   }
 
   if (left === doubleWildcard) {
     // 多分段通配符
-    if (sectionMatch(nextLeft, "", right) === SectionMatchResult.Match) {
+    if (segmentMatch(nextLeft, "", right) === SegmentMatchResult.Match) {
       // **/x 或 **/* 或 **/**
-      return SectionMatchResult.NextMatch;
+      return SegmentMatchResult.NextMatch;
     }
 
-    return SectionMatchResult.Match;
+    return SegmentMatchResult.Match;
   }
 
   if (left === right) {
-    return SectionMatchResult.Match;
+    return SegmentMatchResult.Match;
   }
 
-  return SectionMatchResult.NotMatch;
+  return SegmentMatchResult.NotMatch;
 }
